af4b5e0c40a7bacfa11706a3479b8714fb54bf3d
Rolando <rcruz@jazwings.com>
2017-09-18 14:48:40 -0400

    Login function tested, facebook and google plus missing

--- actions/login.py
+++ actions/login.py
@@ -22,14 +22,17 @@ class Login:
 
         self.result = {}
 
+    def load_elements(self):
+        self.email = self.driver.find_element_by_id('exampleInputEmail1')
+        self.password = self.driver.find_element_by_id('exampleInputPassword1')
+        self.submit = self.driver.find_element_by_id('bt-login')
+
     def login_link(self):
         login_element = self.driver.find_element_by_id('login')
         login_element.click()
         sleep(2)
 
-        self.email = self.driver.find_element_by_id('exampleInputEmail1')
-        self.password = self.driver.find_element_by_id('exampleInputPassword1')
-        self.submit = self.driver.find_element_by_id('bt-login')
+        self.load_elements()
 
         try:
             assert self.driver.current_url == 'https://jazwings.com/login'
@@ -44,20 +47,86 @@ class Login:
         print_result('Login link Succeed')
         print()
 
+    @staticmethod
+    def clear_input(element, content):
+        element.clear()
+        element.send_keys(content)
+
     def invalid_email(self, username, password, task):
-        self.email.send_keys(username)
-        self.password.send_keys(password)
+        self.clear_input(self.email, username)
+        self.clear_input(self.password, password)
         self.submit.send_keys(Keys.RETURN)
 
         wait = WebDriverWait(self.driver, 2)
         alert = wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="toast-container"]')))
+        text = alert.text
+        sleep(5)
         try:
-            assert alert.text == 'Unable to log in with provided credentials.'
+            assert text == 'Unable to log in with provided credentials.'
             self.result[task] = SUCCESS
+            print_result('Invalid email Succeed')
         except AssertionError:
             print_error('Login with random text raises an error')
             self.result[task] = FAIL
         sleep(1)
+        print()
+
+    def valid_login(self, username=False):
+        self.load_elements()
+
+        if username:
+            self.clear_input(self.email, 'testemail')
+        else:
+            self.clear_input(self.email, 'testemail1989@gmail.com')
+        self.clear_input(self.password, '123456abc')
+
+        self.submit.send_keys(Keys.RETURN)
+
+        logged_url = 'https://jazwings.com/discover#discHcont'
+        wait = WebDriverWait(self.driver, 3)
+        alert = wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="toast-container"]')))
+        text = alert.text
+        sleep(5)
+
+        try:
+            assert self.driver.current_url == logged_url
+            assert text == 'cool, have fun!!'
+            self.result['Valid Login'] = SUCCESS
+            if username:
+                print_result('Valid Login with username Succeed')
+            else:
+                print_result('Valid Login with email Succeed')
+        except AssertionError:
+            if username:
+                print_error('Valid Login with username raises an error')
+            else:
+                print_error('Valid Login with email raises an error')
+            self.result['Valid Login'] = FAIL
+        sleep(1)
+        print()
+
+    def logout(self, redirect=True):
+        toggle = self.driver.find_elements_by_xpath('//a[@class="dropdown-toggle"]')[0]
+        toggle.click()
+        links = self.driver.find_elements_by_xpath('//ul[@class="dropdown-menu"]/li/a')
+        for link in links:
+            if link.text == 'Logout':
+                link.click()
+        sleep(5)
+        login = self.driver.find_element_by_id('login')
+        try:
+            assert self.driver.current_url == self.url
+            assert login is not None
+            if redirect:
+                login.click()
+            sleep(1)
+            self.result['Valid Logout'] = SUCCESS
+            print_result('Valid logout Succeed')
+        except AssertionError:
+            print_error('Logout raises an error')
+            self.result['Valid Logout'] = FAIL
+        sleep(1)
+        print()
 
     def start(self):
         self.webdriver.navigate(self.url)
@@ -69,5 +138,12 @@ class Login:
         print_task('Testing invalid email - Valid Username Wrong Password')
         self.invalid_email(username='rolycg89@gmail.com', password='random_password',
                            task='Invalid email - Valid Username Wrong Password')
+        print_task('Testing valid credentials - Email')
+        self.valid_login()
+        print_task('Testing valid logout')
+        self.logout()
+        print_task('Testing valid credentials - username')
+        self.valid_login(username=True)
+        self.logout()
 
         self.driver.quit()
